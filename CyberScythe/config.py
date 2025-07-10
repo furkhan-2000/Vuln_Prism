35518a809d6cb5388da51186be61c60a55aea6ce
furkhan <olx.gamil.com>
2025-07-10 21:54:59 +0530

    *

--- CyberScythe/config.py
+++ CyberScythe/config.py
@@ -24,8 +24,8 @@ AUTH_CREDENTIALS = {
 }
 
 # --- Celery/Redis Settings ---
-REDIS_BROKER_URL = "redis://localhost:6379/0"
-REDIS_BACKEND_URL = "redis://localhost:6379/1"
+REDIS_BROKER_URL = os.getenv("REDIS_BROKER_URL", "redis://localhost:6379/0")
+REDIS_BACKEND_URL = os.getenv("REDIS_BACKEND_URL", "redis://localhost:6379/1")
 
 # --- Continuous Monitoring Settings ---
 MONITORED_SITES = [
