8af9fe06e3f644c5f264bc8b20dbbe8d521b82af
furkhan <olx.gamil.com>
2025-07-22 02:41:24 +0530

    *

--- sast/database.py
+++ sast/database.py
@@ -19,7 +19,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "vulnprism_sast")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 try:
     engine = create_engine(DATABASE_URL, pool_pre_ping=True)
